c1b6bd4abd8e1cdb8b21dbc47f50ad845ce9dd58
iago-711 <iago-506715@educar.rs.gov.br>
2024-10-24 22:01:32 -0300

    Add files via upload

--- gestao-beckend.js
+++ gestao-beckend.js
@@ -1,9 +1,9 @@
 const tabela = 
 [
-    {produto: "Camisa Azul" , tamanho: "G", preco: 20 },
-    {produto: "Camisa verde" , tamanho: "M" , preco: 30 },
-    {produto: "Camisa bege" , tamanho: "P" , preco: 25 },
-    {produto: "Camisa laranja" , tamanho: "G" , preco: 45 }
+    {codigo: 1, produto: "Camisa", cor: "Azul" , tamanho: "G", quantidade: 10, valor: 20 },
+    {codigo: 2, produto: "Camisa", cor: "verde" , tamanho: "M", quantidade: 15, valor: 30 },
+    {codigo: 3, produto: "Camisa", cor: "bege", tamanho: "P", quantidade: 20 , valor: 25 },
+    {codigo: 4, produto: "Camisa", cor: "laranja" , tamanho: "G", quantidade: 30 , valor: 45 }
 ]
 
 function mostrarDados(){
@@ -11,12 +11,40 @@ function mostrarDados(){
     console.table(tabela);
 }
 
-function inserirDado(pProduto, pTamanho, pPreco){
-    tabela.push({produto:pProduto, tamanho:pTamanho, preco:pPreco})
+function inserirDado(pCodigo, pProduto, pCor, pTamanho,pQuantidade, pValor){
+    tabela.push({codigo:pCodigo, produto:pProduto, cor:pCor, tamanho:pTamanho, quantidade:pQuantidade, valor:pValor})
 
 }
 
-module.exports = {mostrarDados, inserirDado}
+function deletarProduto(indice){
+    tabela.splice(indice,1)
+}
+
+function quantiProduto(){
+    let somaProduto = 0
+    for (let i = 0; i < tabela.length; i++){
+        somaProduto=somaProduto+tabela[i].quantidade
+    }
+    return somaProduto
+}
+
+function LocalizarProduto(codigo){
+    
+    let retorno = ''
+    for(let i = 0; i < tabela.length; i++){
+        if(tabela [i].codigo == (codigo)){
+            retorno = "codigo: " + tabela[i].codigo + " - produto: " +tabela[i].produto + " Cor: " +tabela[i].cor + " Tamanho " + tabela[i].tamanho + " Quantidade " + tabela[i].quantidade + " Valor " + tabela[i].valor
+        }
+        else{
+
+        }
+    } 
+    return retorno
+
+}
+
+
+module.exports = {mostrarDados, inserirDado, deletarProduto, quantiProduto, LocalizarProduto}
 
 
 
--- gestao-frontend.js
+++ gestao-frontend.js
@@ -4,11 +4,14 @@ const backend = require("./gestao-beckend.js");
 let menu = ''
 
 function exibirMenu(){
+    
+    
     console.log("1. Adicionar produto")
     console.log("2. Deletar produto")
     console.log("3. listar produtos:")
     console.log("4. Total de estoque: ")
-    console.log("5. Sair")
+    console.log("5. Localizar Produto: ")
+    console.log("6. Sair")
 
     menu = prompt(" Escolha uma opção: ")
             console.log("---------------------")
@@ -17,30 +20,48 @@ function exibirMenu(){
     switch (menu) {
 
         case '1':
-            
-            break;
+        
+        let a = parseInt(prompt("Digite o codigo do produto: "))
+        let b = prompt("Qual produto vc quer inserir? ")
+        let c = prompt("Qual a cor? ")
+        let d = prompt("Qual tamanho? ")
+        let e = parseInt(prompt("Qual a quantidade? "))
+        let f = parseInt(prompt("Qual o valor? "))
+            backend.inserirDado(a,b,c,d,e,f)
+             backend.mostrarDados()
+             console.log("Produto cadastrado com sucesso.")
+                break;
 
         case '2':
             
-
-            break;
+          let g =  prompt ("Qual indice vc quer deletar?")
+           backend.deletarProduto(g);
+            backend.mostrarDados()
+                break;
         case '3':
             backend.mostrarDados()
             
-            break
+                break;
         case '4':
-            
+            console.log(backend.quantiProduto())
             
                 break;
+
         case '5':
-             
+        let Localizar = prompt("Digite o codigo do Produto:")
+          console.log( backend.LocalizarProduto(Localizar))
+
+                break;
+
+        case '6':
+             console.log("Vc saiu!")
 
         default:
             
     }
 
 }
-while(menu !== "5"){
+while(menu !== "6"){
     exibirMenu()
 }
 
